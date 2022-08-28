4c84b185d10a46c131f947760b5c8535a0be6ee0
SK Rajii࿐ <104976581+SadKidBGMZ@users.noreply.github.com>
2022-08-29 03:25:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/SadKidBGMZ/fwdbot /fwdbot 
+  git clone https://github.com/hockbhmv/fwdbot /fwdbot 
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/SadKidBGMZ/fwdbot -b $BRANCH /fwdbot
+  git clone https://github.com/hockbhmv/fwdbot -b $BRANCH /fwdbot
 fi
 cd /fwdbot 
 pip3 install -U -r requirements.txt
