57da731144c5ab8f15e924eb857e9d8fd8d2b27a
SK Rajii࿐ <104976581+SadKidBGMZ@users.noreply.github.com>
2022-08-29 03:40:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SadKidBGMZ/fwdbot.git /fedbot
+  git clone https://github.com/SadKidBGMZ/fwdbot.git /fwdbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone https://github.com/SadKidBGMZ/fwdbot
