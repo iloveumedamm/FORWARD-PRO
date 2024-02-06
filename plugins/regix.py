22e9a5d76d1f352dfe50789d013fdd78cfeb5284
tgaibotz <152408509+tgaibotz@users.noreply.github.com>
2024-02-06 17:48:45 +0500

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -106,7 +106,7 @@ async def pub_(bot, message):
         await edit(m, 'ᴄᴏᴍᴘʟᴇᴛᴇᴅ', "completed", sts) 
         await stop(client, user)
             
-
+async def copy(bot, msg, m, sts):
    try:                                  
      if msg.get("media") and msg.get("caption"):
         await bot.send_cached_media(
